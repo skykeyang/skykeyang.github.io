98bf2df2c8762f378cd7547813aeec3fccbe1064
skykeyang <sohky98@gmail.com>
2023-05-13 03:38:58 +0800

    Minor changes to text and dynamic age
    Made some minor changes to text
    Date is also now dynamic and changes automatically

---
+++ assets/js/derivedValues.js
@@ -0,0 +1,11 @@
+function calculate_age(dob) { 
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms); 
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+var currentAge = calculate_age(new Date(1998, 1, 24));
+var age_element = document.getElementById("age");
+age_element.innerText = currentAge;
+console.log(age_element.innerText);
+
